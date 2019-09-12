7189d1d025cefe60591562dea0b1411e32f0079a
dudekdawid89 <dudekdawid89@gmail.com>
2019-09-11 20:55:04 -0500

    main page UI

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.get("/scrape", function(req, res) {
 // Route for getting all Articles from the db
 app.get("/", function(req, res) {
   // Grab every document in the Articles collection
-  db.Article.find({})
+  db.Article.find().sort({ title: -1 })
     .then(function(dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
       res.render("index",{article: dbArticle});
