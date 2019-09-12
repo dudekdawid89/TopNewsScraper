669c0812a6901ca66130c432a7e74c965392d114
dudekdawid89 <dudekdawid89@gmail.com>
2019-09-11 22:18:19 -0500

    test for heroku

--- models/Article.js
+++ models/Article.js
@@ -24,7 +24,8 @@ var ArticleSchema = new Schema({
     default: false
   },
   image: {
-    type: String
+    type: String,
+    required: true
   },
   // `note` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
